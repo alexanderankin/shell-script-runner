11b2ff42f07e1b2f4db811932ebea5632574dc39
Dave Ankin <alexanderankin@gmail.com>
2016-07-11 13:12:44 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+var shjs = require('shelljs');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET memory status page. */
+router.get('/memory', function(req, res, next) {
+  var memorySummary = shjs.exec('free -m', {
+    silent: true
+  }).stdout;
+
+  var diskSummary = shjs.exec('df -h | grep -v File', {
+    silent: true
+  }).stdout;
+
+  res.render('status', {
+    title: 'Memory Usage Summary',
+    memory: memorySummary,
+    disk: diskSummary
+  });
+});
+
+module.exports = router;
+// require('repl').start('.').context.r = router;
